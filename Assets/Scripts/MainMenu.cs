a7f78f681ee5d34a79a058aba16013ade3b62373
tanisd <50458520+tanisd@users.noreply.github.com>
2021-10-08 20:19:19 -0400

    Added simple main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Invocation: Called on click of 'Play' button
+    //Use: Changes the current scene to the next scene (based on index)
+    public void gameStart()
+    {
+        Debug.Log("Play btn pressed.");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Invocation: Called on click of 'Quit' button
+    //Use: Exits the application
+    public void gameQuit()
+    {
+        Debug.Log("Quit btn pressed.");
+        Application.Quit();
+    }
+}
