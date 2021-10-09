ba0ccd01c18e9ab876ad8e1b8dbfd7a3697ab1d8
tanisd <50458520+tanisd@users.noreply.github.com>
2021-10-09 04:16:45 -0400

    Added simple HP bar, camera, tiles
    -Imported swamp tileset
    -Imported GUI elements
    -Added simple HP bar to HUD
    -Added Cinemachine camera to track player
    -Added a few swamp tiles to SampleScene
    -Moved UI elements into own folder

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 50;
+    public int currHealth;
+    public UIHealthBar HPBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+        HPBar.SetHealthMax(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DEBUG function: damage player w/ Right Shift key
+        if (Input.GetKeyDown(KeyCode.RightShift)) {
+            modifyHealth(-10);
+        }
+    }
+
+    void modifyHealth(int healthMod) {
+        currHealth = currHealth + healthMod;
+        HPBar.SetHealth(currHealth);
+    }
+}
---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public Slider HPSlider;
+
+    //Use: Changes player's maximum HP (on HP bar)
+    public void SetHealthMax(int maxHP)
+    {
+        HPSlider.maxValue = maxHP; //set HP bar's max value to new max HP
+        HPSlider.value = maxHP; //fill HP bar to new max HP
+    }
+
+    //Use: Changes player's current HP (on HP bar)
+    public void SetHealth(int HP)
+    {
+        HPSlider.value = HP;
+    }
+}
