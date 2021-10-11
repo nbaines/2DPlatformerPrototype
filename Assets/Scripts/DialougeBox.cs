c5753492974999d228845ba4ee2aa7dc4a52ed29
tanisd <50458520+tanisd@users.noreply.github.com>
2021-10-11 03:36:23 -0400

    Added sign functionality, HP icon for HP bar
    -Added sign script
    -Added sample sign w/ floating Interact button to SampleScene
    -Added heart icon to HP bar
    -Constrained/froze rotation during player movement

---
+++ Assets/Scripts/DialougeBox.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialougeBox : MonoBehaviour
+{
+    public GameObject uiDialogueBox;
+    public TMP_Text uiDialogueText;
+    public string dialogueActual = "";
+
+    public GameObject uiInteractKey;
+    public bool playerIsInRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsInRange && Input.GetKeyDown(KeyCode.E)) //Player is within range of sign & presses 'E' key
+        {
+            if (uiDialogueBox.activeInHierarchy) //If dialogue box already open, then close it
+            {
+                uiDialogueBox.SetActive(false);
+            }
+            else //If dialogue box not open, then open it & set assigned text
+            {
+                uiDialogueBox.SetActive(true);
+                uiDialogueText.text = dialogueActual;
+            }
+        }
+    }
+
+    //Invocation: Called when another collider w/ a RigidBody2D enters base object's collider
+    //Use: Determine if Player is entering sign's reading range (i.e. collider)
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            playerIsInRange = true;
+        }
+    }
+
+    //Invocation: Called when another collider w/ a RigidBody2D exits base object's collider
+    //Use: Close dialogue box and disable Interaction key when player exits sign's reading range (i.e. collider)
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            playerIsInRange = false;
+            uiDialogueBox.SetActive(false);
+            uiInteractKey.SetActive(false);
+        }
+    }
+
+    //Invocation: Called when another collider w/ a RigidBody2D stays within base object's collider
+    //Use: Show Interaction key (default 'E') if Player stays within sign's reading range (i.e. collider)
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            uiInteractKey.SetActive(true);
+        }
+    }
+}
