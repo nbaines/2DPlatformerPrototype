136742c1802e2122fe0bf14d2d956eecdbba4bd8
Bailey-Merritt <63383959+Bailey-Merritt@users.noreply.github.com>
2021-10-13 00:46:11 -0400

    First Level Built
    The first level is completely built, and on top of that, I've created a death zone for under the level which kills the player when falling down a pit, and I also created a basic spider enemy which moves back and forth.
    
    TODO: Spider enemy still needs animation.

---
+++ Scripts/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerHealth player = other.GetComponent<PlayerHealth>();
+
+        if (player != null)
+        {
+            player.modifyHealth(-50);          
+        }
+    }
+}
